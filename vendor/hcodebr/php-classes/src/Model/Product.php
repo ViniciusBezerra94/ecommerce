ac64bfc71048ef67e1682d0119aaaab25995f4b4
Vinicius Bezerra Lima <vinicius_bezerra@hotmail.com>
2019-09-08 10:50:55 -0300

    Detalhes do Produto

--- vendor/hcodebr/php-classes/src/Model/Product.php
+++ vendor/hcodebr/php-classes/src/Model/Product.php
@@ -140,5 +140,22 @@ class Product extends Model
         $this->checkPhoto();
     }
 
+    public function getFromURL($desurl)
+    {
+        $sql = new Sql();
+        $rows = $sql->select(" SELECT * FROM tb_products where desurl = :desurl", array(
+            ":desurl" => $desurl
+        ));
+
+        $this->setData($rows[0]);
+    }
+
+    public function getCategories()
+    {
+       $sql = new Sql();
+       return $sql->select("SELECT * from tb_categories a INNER JOIN tb_productscategories b 
+       on a.idcategory = b.idcategory where b.idproduct = :idproduct",
+       array (":idproduct" => $this->getidproduct() ) ); 
+    }
 
 }
