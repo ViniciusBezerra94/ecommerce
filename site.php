19467604d2bc2ade033afef311d151f21aef2bf0
Vinicius Bezerra Lima <vinicius_bezerra@hotmail.com>
2019-09-08 10:11:11 -0300

    Paginacao

--- site.php
+++ site.php
@@ -20,13 +20,28 @@ $app->get('/', function() {
 $app->get('/categories/:idcategory',function($idcategory){
 	$category = new Category();
 
+	$page = ( isset($_GET['page']) ) ? (int)$_GET['page'] : 1;
+
 	$category->get( (int) $idcategory );
+
+	$pagination = $category->getProductsPage($page);
+	
+	$pages = [];
+	for ($i=1; $i <= $pagination['pages'] ; $i++) { 
+		array_push($pages, array(
+			'link' => '/hcode_ecommerce/categories/' . $category->getidcategory(). '?page=' . $i,
+			'page' => $i
+		));
+	}
+	
 	$page = new Page();
+	
 
 
 	$page->setTpl("category", array(
 		'category' => $category->getValues(),
-		'products' => Product::checkList($category->getProducts())
+		'products' => Product::checkList($pagination["data"]),
+		'pages' => $pages
 	));
 
 });
