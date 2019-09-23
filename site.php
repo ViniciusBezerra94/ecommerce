9566da1a7b208cc48313ac29b0b80d6ff646b9ee
Vinicius Bezerra Lima <vinicius_bezerra@hotmail.com>
2019-09-22 21:49:24 -0300

    WEB Service Endereco

--- site.php
+++ site.php
@@ -128,16 +128,106 @@ $app->get("/checkout",function(){
 
 	User::verifyLogin(false);
 
+	$address = new Address();
 	$cart = Cart::getFromSession();
 
-	$address = new Address(); 
+	if( !isset($_GET['zipcode']) ){
+		$_GET['zipcode'] = $cart->getdeszipcode();
+	}
+
+
+	if(isset($_GET['zipcode'])) 
+	{
+	
+		$address->loadFromCEP($_GET['zipcode']);
+
+		$cart->setdeszipcode($_GET['zipcode']);
+
+		$cart->save();
+
+		$cart->getCalculateTotal();
+
+	}
+	
+	if(!$address->getdesaddress()) $address->setdesaddress("");
+	if(!$address->getdescomplement()) $address->setdescomplement("");
+	if(!$address->getdesdistrict()) $address->setdesdistrict("");
+	if(!$address->getdescity()) $address->setdescity("");
+	if(!$address->getdesstate()) $address->setdesstate("");
+	if(!$address->getdescountry()) $address->setdescountry("");
+	if(!$address->getdeszipcode()) $address->setdeszipcode("");
+
 
 	$page = new Page();
 	$page->setTpl("checkout", array(
 		'cart' => $cart->getValues(),
-		'address' => $address->getValues()
+		'address' => $address->getValues(),
+		'products'=> $cart->getProducts(),
+		'error'=>Address::getMsgError()
+
 	));
 });
+
+$app->post("/checkout",function(){
+
+	User::verifyLogin(false);
+
+	if( !isset($_POST['zipcode']) || $_POST['zipcode'] )
+	{
+		Address::setMsgError("Informe o Cep. ");
+		header("Location: /hcode_ecommerce/checkout");
+		exit;
+	}
+
+	if( !isset($_POST['desaddress']) || $_POST['desaddress'] )
+	{
+		Address::setMsgError("Informe o endereço. ");
+		header("Location: /hcode_ecommerce/checkout");
+		exit;
+	}
+	if( !isset($_POST['desdestrict']) || $_POST['desdestrict'] )
+	{
+		Address::setMsgError("Informe o bairro. ");
+		header("Location: /hcode_ecommerce/checkout");
+		exit;
+	}
+	if( !isset($_POST['descity']) || $_POST['descity'] )
+	{
+		Address::setMsgError("Informe a cidade. ");
+		header("Location: /hcode_ecommerce/checkout");
+		exit;
+	}
+
+	if( !isset($_POST['desstate']) || $_POST['desstate'] )
+	{
+		Address::setMsgError("Informe o estado. ");
+		header("Location: /hcode_ecommerce/checkout");
+		exit;
+	}
+	if( !isset($_POST['descountry']) || $_POST['descountry'] )
+	{
+		Address::setMsgError("Informe o país. ");
+		header("Location: /hcode_ecommerce/checkout");
+		exit;
+	}
+
+
+	$user = User::getFromSession();
+
+	$address = new Address();
+
+	$_POST['deszipcode'] = $_POST['zipcode'];
+	$_POST['idperson'] = $user->getidperson();
+	$address->setData($_POST);
+	$address->save();
+
+	header("Location: /hcode_ecommerce/order");
+	exit;
+
+	
+
+});
+
 
 $app->get("/login",function(){
 
@@ -333,7 +423,7 @@ $app->post("/profile",function(){
 
 
 	$_POST['inadmin'] = $user->getinadmin();
-	$_POST['despassword'] = $user->getpassword();
+	$_POST['despassword'] = $user->getdespassword();
 	$_POST['deslogin'] = $_POST['desemail'];
 	$user->setData($_POST);
 
