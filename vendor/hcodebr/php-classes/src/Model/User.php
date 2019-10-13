2412834f6c05b9239d70b5859d7735a38bb0c85a
Vinicius Bezerra Lima <vinicius_bezerra@hotmail.com>
2019-10-13 18:08:06 -0300

    Admin - User - Search And Pagination

--- vendor/hcodebr/php-classes/src/Model/User.php
+++ vendor/hcodebr/php-classes/src/Model/User.php
@@ -334,4 +334,63 @@ class User extends Model
 
     }
 
+    public static function getPage($page = 1, $itemsPerPage = 3 )
+    {
+
+        $start = ($page -1 ) * $itemsPerPage;
+
+        $sql = new Sql();
+
+        $results = $sql->select("SELECT SQL_CALC_FOUND_ROWS *
+        from tb_users as a 
+        inner join tb_persons b using(idperson) 
+        order by b.desperson desc
+        LIMIT $start, $itemsPerPage;
+        ");
+
+        $resultTotal = $sql->select("Select FOUND_ROWS() as nrtotal;");
+
+
+
+        return array(
+            'data'=> $results,
+            'total' => (int) $resultTotal[0]['nrtotal'],
+            'pages' => ceil( $resultTotal[0]['nrtotal'] / $itemsPerPage)
+
+        );
+
+    }
+
+    public static function getPageSearch($search, $page = 1, $itemsPerPage = 3 )
+    {
+
+        $start = ($page -1 ) * $itemsPerPage;
+
+        $sql = new Sql();
+
+        $results = $sql->select("SELECT SQL_CALC_FOUND_ROWS *
+        from tb_users as a 
+        inner join tb_persons b using(idperson) 
+        where b.desperson like :search or b.desemail = :search or a.deslogin like :search
+        order by b.desperson desc
+        LIMIT $start, $itemsPerPage;
+        ", array(
+            'search' => '%' . $search . '%'
+        ));
+
+        $resultTotal = $sql->select("Select FOUND_ROWS() as nrtotal;");
+
+
+
+        return array(
+            'data'=> $results,
+            'total' => (int) $resultTotal[0]['nrtotal'],
+            'pages' => ceil( $resultTotal[0]['nrtotal'] / $itemsPerPage)
+
+        );
+
+    }
+
+
+
 }
